cb5b8713b9232625d9c87745e1febc5c5871cc95
casperbot2 <aliahmd2233445t@gmail.com>
2019-11-14 13:35:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const client2 = new Discord.Client();
 console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("حط الاي دي حق الروم اللي يحصل بيها الاسبام هنا")
+let channel =     client.channels.get("644471173631049729")
 setInterval(function() {
-channel.send(`حط الكلام اللي تبغاه هنا`);
+channel.send(`يا عباس يا عباس يا عباس يا عباس`);
+}, 30)
+})
+
+client.on("ready", () => {
+let channel =     client.channels.get("644471173631049729")
+setInterval(function() {
+channel.send(`يا عباس يا عباس يا عباس يا عباس`);
 }, 30)
 })
 
 client.login(process.env.BOT_TOKEN);
+client2.login(process.env.BOT_TOKEN2);
